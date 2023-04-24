0c15c5967ffea86be1454d21e6ade1f5a6d9c809
Kenan Casey <kenancasey@gmail.com>
2023-04-24 17:33:12 -0500

    Add excuses

--- generator.py
+++ generator.py
@@ -1,10 +1,24 @@
 import os
 
-def adder(i1, i2):
-    return (i1 + i2)
-
 def main():
-    print("")
+    filename = "excuses.txt"
+    print(f"Reading {filename}")
+
+    with open(filename, "r") as file:
+        raw_lines = file.readlines()
+
+    lines = []
+    count = 0
+    for line in raw_lines:
+        clean = str.strip( line )
+        lines.append( clean  )
+        print(f"line {count}: " + line)
+        count = count + 1
+
+        # if line is blank, skip it
+        # if line starts with a #, skip it
+        # remove \n from each line
+
 
 
 if __name__ == "__main__":
